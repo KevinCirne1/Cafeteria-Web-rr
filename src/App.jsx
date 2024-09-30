179315e2eb0dd853b0b94804a99e12d9ed0ff6f5
KevinCirne1 <121322605+KevinCirne1@users.noreply.github.com>
2024-09-30 12:50:21 -0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-
-
-function App() {
+const App = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <main>
+        <Outlet />  {/* Renderiza as rotas filhas aqui */}
+      </main>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
